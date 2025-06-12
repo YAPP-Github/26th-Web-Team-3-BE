8f62675d5a8dc62d2ca7420977bdc7d6207b7ca1
Jhzlo <kjh0010703@naver.com>
2025-06-13 01:16:29 +0900

    feat: api error message resolver 설정

---
+++ src/main/kotlin/com/yapp/demo/common/exception/MessageResolver.kt
@@ -0,0 +1,18 @@
+package com.yapp.demo.common.exception
+
+import java.text.MessageFormat
+import java.util.*
+
+object MessageResolver {
+    private const val BUNDLE_NAME = "api-error-messages"
+    private val bundle: ResourceBundle = ResourceBundle.getBundle(BUNDLE_NAME)
+
+    fun resolve(code: String, args: Array<out Any>? = null): String? {
+        return try {
+            val pattern = bundle.getString(code)
+            if (args.isNullOrEmpty()) pattern else MessageFormat.format(pattern, *args)
+        } catch (e: MissingResourceException) {
+            null
+        }
+    }
+}
