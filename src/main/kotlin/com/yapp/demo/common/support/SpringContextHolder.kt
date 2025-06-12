139c74a87cd588d739aef549a121f9c28b888cb5
Jhzlo <kjh0010703@naver.com>
2025-06-13 01:15:45 +0900

    feat: SpringContextHolder 헬퍼 객체 생성

---
+++ src/main/kotlin/com/yapp/demo/common/support/SpringContextHolder.kt
@@ -0,0 +1,16 @@
+package com.yapp.demo.common.support
+
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.stereotype.Component
+
+@Component
+object SpringContextHolder : ApplicationContextAware {
+    private lateinit var context: ApplicationContext
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        context = applicationContext
+    }
+
+    fun <T> getBean(clazz: Class<T>): T = context.getBean(clazz)
+}
