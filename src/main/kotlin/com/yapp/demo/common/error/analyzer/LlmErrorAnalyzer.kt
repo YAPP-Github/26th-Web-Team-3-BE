60bb0ee4037441b4d9e97c09d1d3f562e2c11c30
Jhzlo <kjh0010703@naver.com>
2025-06-23 23:32:00 +0900

    feat: LLM Error Anlayzer 요청 로직 생성

---
+++ src/main/kotlin/com/yapp/demo/common/error/analyzer/LlmErrorAnalyzer.kt
@@ -0,0 +1,125 @@
+package com.yapp.demo.common.error.analyzer
+
+import com.yapp.demo.common.error.analyzer.dto.AnalyzeErrorRequest
+import com.yapp.demo.common.error.analyzer.dto.AnalyzeErrorResponse
+import com.yapp.demo.common.error.analyzer.dto.MethodSignatureInfo
+import com.yapp.demo.common.error.analyzer.dto.ParameterInfo
+import com.yapp.demo.infrastructure.llm.LlmProperties
+import com.yapp.demo.infrastructure.llm.call
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.LineNumberNode
+import org.objectweb.asm.tree.MethodNode
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@Component
+class LlmErrorAnalyzer(
+    @Qualifier("llmWebClient")
+    private val llmWebClient: WebClient,
+    private val llmProperties: LlmProperties,
+) : ErrorAnalyzer {
+    override fun analyze(request: AnalyzeErrorRequest): AnalyzeErrorResponse {
+        val filteredStackTrace = filterStackTrace(request.exception)
+
+        return llmWebClient.call(
+            method = HttpMethod.POST,
+            uri = "/api/v1/prediction/${llmProperties.id}",
+            requestBody =
+            mapOf(
+                "question" to
+                    """
+                        service: ${llmProperties.serviceName},
+                        httpMethod: ${request.httpMethod.lowercase()},
+                        requestUrl: ${request.path},
+                        cause: ${getStackTraceAsString(request.exception.message!!, filteredStackTrace)},
+                        methodSignatures: ${getMethodSignatures(filteredStackTrace)}
+                        """.trimIndent(),
+            ),
+            headersConsumer = { it.setBearerAuth(llmProperties.key) },
+        )
+    }
+
+    // 외부 라이브러리 예외는 제외
+    private fun filterStackTrace(exception: Exception): List<StackTraceElement> {
+        return exception.stackTrace.filter { it.className.startsWith(BASE_PACKAGE_PREFIX) }
+    }
+
+    private fun getStackTraceAsString(
+        message: String,
+        stackTrace: List<StackTraceElement>,
+    ): String {
+        return buildString {
+            appendLine(message)
+            stackTrace.forEach { appendLine(it) }
+        }
+    }
+
+    private fun getMethodSignatures(stackTraceElements: List<StackTraceElement>): List<MethodSignatureInfo> {
+        return stackTraceElements.mapNotNull { getMethodSignature(it.className, it.lineNumber) }
+    }
+
+    private fun getMethodSignature(
+        className: String,
+        lineToFind: Int,
+    ): MethodSignatureInfo? {
+        val classInputStream =
+            this.javaClass.classLoader
+                .getResourceAsStream(className.replace('.', '/') + ".class")
+                ?: return null
+
+        val classNode = ClassNode()
+        val classReader = ClassReader(classInputStream)
+        classReader.accept(classNode, 0)
+
+        for (method in classNode.methods) {
+            val lineNumbers = mutableListOf<Int>()
+
+            for (insn in method.instructions) {
+                if (insn is LineNumberNode) {
+                    lineNumbers.add(insn.line)
+                }
+            }
+
+            if (lineToFind in lineNumbers) {
+                return extractMethodSignature(className, lineToFind, method)
+            }
+        }
+        return null
+    }
+
+    private fun extractMethodSignature(
+        className: String,
+        lineNumber: Int,
+        method: MethodNode,
+    ): MethodSignatureInfo {
+        val argumentTypes = Type.getArgumentTypes(method.desc)
+
+        val parameterNames =
+            method.localVariables
+                .asSequence()
+                .drop(1)
+                .take(argumentTypes.size)
+                .map { it.name }
+                .toList()
+
+        val parameters =
+            argumentTypes.mapIndexed { index, type ->
+                ParameterInfo(parameterNames.getOrNull(index), type.className)
+            }
+
+        return MethodSignatureInfo(
+            className = className,
+            lineNumber = lineNumber,
+            parameters = parameters,
+            returnType = Type.getReturnType(method.desc).className,
+        )
+    }
+
+    companion object {
+        private const val BASE_PACKAGE_PREFIX = "com.yapp"
+    }
+}
