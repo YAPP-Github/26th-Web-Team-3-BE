11c63d9596ffe988e8ba490dd3788ca13b95cced
Jhzlo <kjh0010703@naver.com>
2025-06-23 20:35:26 +0900

    chore: flowise + llm config

---
+++ src/main/kotlin/com/yapp/demo/config/LlmConfiguration.kt
@@ -0,0 +1,23 @@
+package com.yapp.demo.config
+
+import com.yapp.demo.infrastructure.llm.LlmProperties
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+@EnableConfigurationProperties(LlmProperties::class)
+class LlmConfiguration {
+    @Bean
+    @Qualifier("llmWebClient")
+    fun llmWebClient(llmProperties: LlmProperties): WebClient {
+        return WebClient.builder()
+            .baseUrl(llmProperties.baseUrl)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build()
+    }
+}
