f3b6a2dfad09d2326adfa76cc9a01598302177cb
Jhzlo <kjh0010703@naver.com>
2025-06-24 00:03:08 +0900

    chore: mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.13.10")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
