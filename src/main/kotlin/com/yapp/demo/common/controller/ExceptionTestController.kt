90420fa0911c783336aff457accae3aff0ae4c39
Jhzlo <kjh0010703@naver.com>
2025-06-25 01:44:45 +0900

    chore: prod 환경에서 제외하도록 설정

--- src/main/kotlin/com/yapp/demo/common/controller/ExceptionTestController.kt
+++ src/main/kotlin/com/yapp/demo/common/controller/ExceptionTestController.kt
@@ -1,8 +1,10 @@
-package com.yapp.demo
+package com.yapp.demo.common.controller
 
+import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Profile("!prod")
 @RestController
 class ExceptionTestController {
     @GetMapping("/test/exception")
