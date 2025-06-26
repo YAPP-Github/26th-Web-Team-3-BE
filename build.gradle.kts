dbf63641fb55224f9a46fa656010af0a3f76e70b
Jhzlo <kjh0010703@naver.com>
2025-06-26 21:36:51 +0900

    chore: prometheus 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+    // prometheus
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
