e6dad71bbac1b78a8bd66d665cc591952f2b76f2
HeoSeoKMun <gnivy303@gmail.com>
2025-06-29 17:00:31 +0900

    chore: swagger authorization 설정 추가

---
+++ src/main/kotlin/com/yapp/demo/config/SwaggerConfig.kt
@@ -0,0 +1,36 @@
+package com.yapp.demo.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val securitySchemeName = "Authorization"
+
+        val apiInfo =
+            Info()
+                .title("API Documentation")
+                .description("API documentation with JWT Authorization")
+                .version("1.0")
+
+        return OpenAPI()
+            .components(
+                Components().addSecuritySchemes(
+                    securitySchemeName,
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"),
+                ),
+            ).addSecurityItem(
+                SecurityRequirement().addList(securitySchemeName),
+            ).info(apiInfo)
+    }
+}
