1380c269953fa88daa65b228cbf8b85dc523d9f7
Jhzlo <kjh0010703@naver.com>
2025-07-09 00:56:24 +0900

    feat: TimeCapsule 엔티티 생성

---
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/entity/TimeCapsule.kt
@@ -0,0 +1,41 @@
+package com.yapp.lettie.domain.timecapsule.entity
+
+
+import com.yapp.lettie.domain.BaseEntity
+import com.yapp.lettie.domain.timecapsule.entity.vo.AccessType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "time_capsule")
+class TimeCapsule(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "invite_code", nullable = false, unique = true)
+    var inviteCode: String,
+
+    @Column(name = "title", nullable = false)
+    var title: String,
+
+    @Column(name = "subtitle")
+    var subtitle: String? = null,
+
+    @Column(name = "access_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    var accessType: AccessType,
+
+    @Column(name = "open_at", nullable = false)
+    val openAt: LocalDateTime,
+
+    @Column(name = "closed_at", nullable = false)
+    var closedAt: LocalDateTime,
+): BaseEntity()
