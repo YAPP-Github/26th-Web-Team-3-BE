3efd61eca66137537a37d64d520a1b2e25f80822
Jhzlo <kjh0010703@naver.com>
2025-07-09 01:02:25 +0900

    feat: TimeCapsuleLike Repository 생성

---
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleLikeRepository.kt
@@ -0,0 +1,9 @@
+package com.yapp.lettie.domain.timecapsule.repository
+
+import com.yapp.lettie.domain.timecapsule.entity.TimeCapsuleLike
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface TimeCapsuleLikeRepository : JpaRepository<TimeCapsuleLike, Long> {
+    fun findByUserId(userId: Long): List<TimeCapsuleLike>
+    fun findByCapsuleId(capsuleId: Long): List<TimeCapsuleLike>
+}
