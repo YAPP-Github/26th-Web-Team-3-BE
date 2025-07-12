d02afe3e4d069e66831f905a83c10582cbbdfcf4
Jhzlo <kjh0010703@naver.com>
2025-07-12 19:59:17 +0900

    feat: timeCapsule 생성 로직 구성

---
+++ src/main/kotlin/com/yapp/lettie/api/timecapsule/service/TimeCapsuleService.kt
@@ -0,0 +1,35 @@
+package com.yapp.lettie.api.timecapsule.service
+
+import com.yapp.lettie.api.timecapsule.controller.request.CreateTimeCapsuleRequest
+import com.yapp.lettie.api.timecapsule.service.dto.CreateTimeCapsulePayload
+import com.yapp.lettie.api.timecapsule.service.writer.TimeCapsuleWriter
+import com.yapp.lettie.api.user.service.reader.UserReader
+import com.yapp.lettie.domain.timecapsule.entity.TimeCapsule
+import com.yapp.lettie.domain.timecapsule.entity.TimeCapsuleUser
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TimeCapsuleService(
+    private val userReader: UserReader,
+    private val timeCapsuleWriter: TimeCapsuleWriter,
+) {
+    fun createTimeCapsule(
+        userId: Long,
+        payload: CreateTimeCapsulePayload,
+    ) {
+        val capsule = TimeCapsule.of(generateInviteCode(), payload)
+        val timeCapsuleUser = TimeCapsuleUser.of(userReader.getById(userId), capsule)
+        capsule.timeCapsuleUsers.add(timeCapsuleUser)
+
+        timeCapsuleWriter.save(capsule)
+    }
+
+    private fun generateInviteCode(): String {
+        return UUID.randomUUID().toString().take(RANDOM_VALUE_LENGTH)
+    }
+
+    companion object {
+        private const val RANDOM_VALUE_LENGTH = 8
+    }
+}
