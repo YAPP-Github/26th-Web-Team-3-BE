ec0188c1b72a0b25fb8e659bde5dda222474fe60
Jhzlo <kjh0010703@naver.com>
2025-07-12 20:48:27 +0900

    feat: timeCapsule Reader 생성

--- src/main/kotlin/com/yapp/lettie/api/timecapsule/service/reader/TimeCapsuleReader.kt
+++ src/main/kotlin/com/yapp/lettie/api/timecapsule/service/reader/TimeCapsuleReader.kt
@@ -1,3 +1,24 @@
 package com.yapp.lettie.api.timecapsule.service.reader
 
-class TimeCapsuleReader
+import com.yapp.lettie.common.error.ErrorMessages
+import com.yapp.lettie.common.exception.ApiErrorException
+import com.yapp.lettie.domain.timecapsule.entity.TimeCapsule
+import com.yapp.lettie.domain.timecapsule.repository.TimeCapsuleRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class TimeCapsuleReader(
+    private val timeCapsuleRepository: TimeCapsuleRepository,
+) {
+    @Transactional(readOnly = true)
+    fun getById(id: Long): TimeCapsule {
+        return findById(id) ?: throw ApiErrorException(ErrorMessages.CAPSULE_NOT_FOUND)
+    }
+
+    @Transactional(readOnly = true)
+    fun findById(id: Long): TimeCapsule? {
+        return timeCapsuleRepository.findByIdOrNull(id)
+    }
+}
