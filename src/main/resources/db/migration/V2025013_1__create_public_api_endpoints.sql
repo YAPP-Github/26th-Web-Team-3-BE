bf7b5275d575152c588cfc4ae5cc44e0b1b91e1a
HeoSeoKMun <gnivy303@gmail.com>
2025-07-13 19:06:54 +0900

    Chore: sql 문 수정

--- src/main/resources/db/migration/V2025013_1__create_public_api_endpoints.sql
+++ src/main/resources/db/migration/V2025013_1__create_public_api_endpoints.sql
@@ -3,7 +3,7 @@ CREATE TABLE api_auth
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     path       VARCHAR(255)                  NOT NULL,
     method     VARCHAR(10)                   NOT NULL,
-    auth_type  ENUM ('REQUIRED', 'OPTIONAL') NOT NULL,
+    auth_type  ENUM ('REQUIRED', 'OPTIONAL', 'NONE') NOT NULL,
     created_at DATETIME(6)                   NOT NULL,
     updated_at DATETIME(6)                   NOT NULL
 );
