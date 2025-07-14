94de98dc04847d55315bba93b43f1733a0334346
HeoSeoKMun <gnivy303@gmail.com>
2025-07-14 21:51:19 +0900

    Refactor: 해당 부분 삭제

--- src/main/resources/db/migration/V2025013_1__create_public_api_endpoints.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE api_auth
-(
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    path       VARCHAR(255)                  NOT NULL,
-    method     VARCHAR(10)                   NOT NULL,
-    auth_type  ENUM ('REQUIRED', 'OPTIONAL', 'NONE') NOT NULL,
-    created_at DATETIME(6)                   NOT NULL,
-    updated_at DATETIME(6)                   NOT NULL
-);
