27006093c1ae6d8f88cf920be9ca97305747a91d
Jhzlo <kjh0010703@naver.com>
2025-07-18 00:14:15 +0900

    style: ktlint format

--- src/main/kotlin/com/yapp/lettie/api/timecapsule/controller/TimeCapsuleDetailSwagger.kt
+++ src/main/kotlin/com/yapp/lettie/api/timecapsule/controller/TimeCapsuleDetailSwagger.kt
@@ -15,7 +15,7 @@ interface TimeCapsuleDetailSwagger {
     @Operation(
         summary = "타임캡슐 상세 조회",
         description =
-        """
+            """
         캡슐의 기본 정보와 오픈일시, 참여자 수, 좋아요 여부, 상태 및 남은 시간을 조회합니다.
         로그인하지 않은 사용자도 접근 가능하며, 로그인 사용자일 경우 좋아요 여부(liked)가 포함됩니다.
 
@@ -23,7 +23,7 @@ interface TimeCapsuleDetailSwagger {
         - 작성 마감 시점이 지나지 않은 경우: status = WRITABLE, remainingTime = closedAt - 현재
         - 작성이 마감되고 캡슐 오픈까지 남은 경우: status = WAITING_OPEN, remainingTime = openedAt - 현재
         - 타임캡슐이 오픈된 경우: status = OPENED, remainingTime = openedAt
-        """
+        """,
     )
     fun getCapsuleDetail(
         @Parameter(hidden = true) userInfo: UserInfoDto?,
