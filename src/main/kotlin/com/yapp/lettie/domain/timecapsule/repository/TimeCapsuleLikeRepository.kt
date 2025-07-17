8d4a540dc96c7b1b7b9773231e7e09a739a8fd46
Jhzlo <kjh0010703@naver.com>
2025-07-17 20:47:51 +0900

    feat: timeCapsuleLike jpa 메서스 수정

--- src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleLikeRepository.kt
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleLikeRepository.kt
@@ -1,10 +1,11 @@
 package com.yapp.lettie.domain.timecapsule.repository
 
+import com.yapp.lettie.domain.timecapsule.entity.TimeCapsule
 import com.yapp.lettie.domain.timecapsule.entity.TimeCapsuleLike
+import com.yapp.lettie.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TimeCapsuleLikeRepository : JpaRepository<TimeCapsuleLike, Long> {
-    fun findByUserId(userId: Long): List<TimeCapsuleLike>
-
-    fun findByTimeCapsuleId(capsuleId: Long): List<TimeCapsuleLike>
+    fun findByUserAndTimeCapsule(user: User, capsule: TimeCapsule): TimeCapsuleLike?
+    fun findByUserAndIsLikedTrue(user: User): List<TimeCapsuleLike>
 }
