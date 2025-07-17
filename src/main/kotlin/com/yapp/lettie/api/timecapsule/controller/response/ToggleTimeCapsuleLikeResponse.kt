d98247bdb2230b881b6a583a69875bbffb537fb8
Jhzlo <kjh0010703@naver.com>
2025-07-17 20:46:58 +0900

    feat: timeCapsule 좋아요 response dto 생성

---
+++ src/main/kotlin/com/yapp/lettie/api/timecapsule/controller/response/ToggleTimeCapsuleLikeResponse.kt
@@ -0,0 +1,8 @@
+package com.yapp.lettie.api.timecapsule.controller.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class ToggleTimeCapsuleLikeResponse (
+    @Schema(description = "timeCapsule Like", example = "true")
+    val isLiked: Boolean
+)
