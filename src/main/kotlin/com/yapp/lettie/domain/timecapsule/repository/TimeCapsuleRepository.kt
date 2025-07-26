2642da56e141414303c2d153dbbac85befbd8a27
Jhzlo <kjh0010703@naver.com>
2025-07-26 16:06:00 +0900

    feat: 내가 만든 타임캡슐 리스트 불러오기

--- src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleRepository.kt
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleRepository.kt
@@ -1,8 +1,14 @@
 package com.yapp.lettie.domain.timecapsule.repository
 
 import com.yapp.lettie.domain.timecapsule.entity.TimeCapsule
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TimeCapsuleRepository : JpaRepository<TimeCapsule, Long> {
     fun findByInviteCode(inviteCode: String): TimeCapsule?
+
+    fun findByCreatorIdOrderByCreatedAtDesc(
+        creatorId: Long,
+        pageable: Pageable,
+    ): List<TimeCapsule>?
 }
