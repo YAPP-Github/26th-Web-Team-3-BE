c8338482f624dd4bf1a25b281b78cb335c428acd
Jhzlo <kjh0010703@naver.com>
2025-07-26 17:48:19 +0900

    feat: 편지수, 생성일자 순으로 정렬 jpql 쿼리 작성

--- src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleRepository.kt
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleRepository.kt
@@ -3,6 +3,9 @@ package com.yapp.lettie.domain.timecapsule.repository
 import com.yapp.lettie.domain.timecapsule.entity.TimeCapsule
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDateTime
 
 interface TimeCapsuleRepository : JpaRepository<TimeCapsule, Long> {
     fun findByInviteCode(inviteCode: String): TimeCapsule?
@@ -11,4 +14,14 @@ interface TimeCapsuleRepository : JpaRepository<TimeCapsule, Long> {
         creatorId: Long,
         pageable: Pageable,
     ): List<TimeCapsule>?
+
+    @Query(
+        """
+    SELECT tc FROM TimeCapsule tc
+    LEFT JOIN Letter l ON l.timeCapsule = tc
+    GROUP BY tc.id
+    ORDER BY COUNT(l.id) DESC, tc.createdAt DESC
+    """
+    )
+    fun findPopularTimeCapsules(pageable: Pageable): List<TimeCapsule>
 }
