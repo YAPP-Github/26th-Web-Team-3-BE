148f742a5ca5a37a9dca8681fd1c951d586617bb
HeoSeoKMun <gnivy303@gmail.com>
2025-07-26 16:12:07 +0900

    add: 편지 조회 기능 구현

--- src/main/kotlin/com/yapp/lettie/api/letter/controller/swagger/LetterSwagger.kt
+++ src/main/kotlin/com/yapp/lettie/api/letter/controller/swagger/LetterSwagger.kt
@@ -1,13 +1,17 @@
 package com.yapp.lettie.api.letter.controller.swagger
 
-import com.yapp.lettie.api.auth.annotation.LoginUser
 import com.yapp.lettie.api.letter.controller.request.CreateLetterRequest
 import com.yapp.lettie.api.letter.controller.response.CreateLetterResponse
+import com.yapp.lettie.api.letter.controller.response.LettersResponse
 import com.yapp.lettie.common.dto.ApiResponse
-import com.yapp.lettie.common.dto.UserInfoDto
+import com.yapp.lettie.common.dto.UserInfoPayload
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.Parameters
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestBody
 
 interface LetterSwagger {
     @Operation(
@@ -15,7 +19,34 @@ interface LetterSwagger {
         description = "캡슐에 편지를 작성합니다.",
     )
     fun writeLetter(
-        @LoginUser userInfo: UserInfoDto,
-        @RequestBody request: CreateLetterRequest,
+        userInfo: UserInfoPayload,
+        request: CreateLetterRequest,
     ): ResponseEntity<ApiResponse<CreateLetterResponse>>
+
+    @Operation(
+        summary = "타임캡슐 편지 조회",
+        description = "타임캡슐에 작성된 편지를 조회합니다.",
+    )
+    @Parameters(
+        Parameter(
+            name = "page",
+            description = "페이지 번호 (0부터 시작)",
+            `in` = ParameterIn.QUERY,
+        ),
+        Parameter(
+            name = "size",
+            description = "페이지 크기",
+            `in` = ParameterIn.QUERY,
+        ),
+        Parameter(
+            name = "sort",
+            description = "정렬 조건 (예: id,desc 또는 id,asc)",
+            `in` = ParameterIn.QUERY,
+        ),
+    )
+    fun readLetter(
+        userInfo: UserInfoPayload,
+        capsuleId: Long,
+        @ParameterObject pageable: Pageable,
+    ): ResponseEntity<ApiResponse<LettersResponse>>
 }
