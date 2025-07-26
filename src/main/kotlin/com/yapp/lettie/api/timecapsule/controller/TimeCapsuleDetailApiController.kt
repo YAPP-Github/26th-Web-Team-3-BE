7d935fe95f766e9b1d014e27372ebd1eaf48e2db
Jhzlo <kjh0010703@naver.com>
2025-07-26 16:07:10 +0900

    feat: 내가 만든 타임캡슐 리스트 조회 api 생성

--- src/main/kotlin/com/yapp/lettie/api/timecapsule/controller/TimeCapsuleDetailApiController.kt
+++ src/main/kotlin/com/yapp/lettie/api/timecapsule/controller/TimeCapsuleDetailApiController.kt
@@ -2,6 +2,7 @@ package com.yapp.lettie.api.timecapsule.controller
 
 import com.yapp.lettie.api.auth.annotation.LoginUser
 import com.yapp.lettie.api.timecapsule.controller.response.TimeCapsuleDetailResponse
+import com.yapp.lettie.api.timecapsule.controller.response.TimeCapsuleSummaryResponse
 import com.yapp.lettie.api.timecapsule.controller.swagger.TimeCapsuleDetailSwagger
 import com.yapp.lettie.api.timecapsule.service.TimeCapsuleDetailService
 import com.yapp.lettie.common.dto.ApiResponse
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -21,15 +23,22 @@ class TimeCapsuleDetailApiController(
     override fun getCapsuleDetail(
         @LoginUser userInfo: UserInfoDto,
         @PathVariable capsuleId: Long,
-    ): ResponseEntity<ApiResponse<TimeCapsuleDetailResponse>> =
+    ): ResponseEntity<ApiResponse<TimeCapsuleDetailResponse>> {
+        val detailDto = timeCapsuleDetailService.getTimeCapsuleDetail(capsuleId, userInfo.id)
+        return ResponseEntity.ok(
+            ApiResponse.success(TimeCapsuleDetailResponse.from(detailDto)),
+        )
+    }
+
+    @GetMapping("/my")
+    override fun getMyTimeCapsules(
+        @LoginUser userInfo: UserInfoDto,
+        @RequestParam limit: Int,
+    ): ResponseEntity<ApiResponse<List<TimeCapsuleSummaryResponse>>> =
         ResponseEntity.ok(
             ApiResponse.success(
-                TimeCapsuleDetailResponse.from(
-                    timeCapsuleDetailService.getTimeCapsuleDetail(
-                        capsuleId = capsuleId,
-                        userId = userInfo.id,
-                    ),
-                ),
+                timeCapsuleDetailService.getMyTimeCapsules(userInfo.id, limit)
+                    .map { TimeCapsuleSummaryResponse.from(it) },
             ),
         )
 }
