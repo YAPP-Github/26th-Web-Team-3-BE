a5a75087f5c47d0b1fa81c96f0043b4a95ff91b7
HeoSeoKMun <gnivy303@gmail.com>
2025-07-26 16:11:14 +0900

    style: 코드 스타일 변경

--- src/main/kotlin/com/yapp/lettie/common/dto/UserInfoPayload.kt
+++ src/main/kotlin/com/yapp/lettie/common/dto/UserInfoPayload.kt
@@ -3,7 +3,7 @@ package com.yapp.lettie.common.dto
 import io.swagger.v3.oas.annotations.Hidden
 
 @Hidden
-data class UserInfoDto(
+data class UserInfoPayload(
     val id: Long,
     val roles: List<String>,
 )
