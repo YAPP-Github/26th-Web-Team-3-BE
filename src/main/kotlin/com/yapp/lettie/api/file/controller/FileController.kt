44d471ce20c23d381c9cc0619a6b232f5d2ec0ea
Jhzlo <kjh0010703@naver.com>
2025-08-01 12:18:06 +0900

    chore: swagger 문서 수정

--- src/main/kotlin/com/yapp/lettie/api/file/controller/FileController.kt
+++ src/main/kotlin/com/yapp/lettie/api/file/controller/FileController.kt
@@ -4,7 +4,6 @@ import com.yapp.lettie.api.file.controller.response.PresignedUrlResponse
 import com.yapp.lettie.api.file.controller.swagger.FileSwagger
 import com.yapp.lettie.api.file.service.FileService
 import com.yapp.lettie.domain.file.FileType
-import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "File", description = "파일 업로드/다운로드 API")
 @RestController
 @RequestMapping("/api/v1/files")
 class FileController(
