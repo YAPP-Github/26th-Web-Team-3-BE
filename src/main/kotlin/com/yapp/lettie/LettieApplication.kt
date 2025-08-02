502d144a882f50610824785401d8b463251709bb
Jhzlo <kjh0010703@naver.com>
2025-08-02 21:20:04 +0900

    chore: enable scheduling

--- src/main/kotlin/com/yapp/lettie/LettieApplication.kt
+++ src/main/kotlin/com/yapp/lettie/LettieApplication.kt
@@ -3,9 +3,11 @@ package com.yapp.lettie
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 class LettieApplication
 
 fun main(args: Array<String>) {
