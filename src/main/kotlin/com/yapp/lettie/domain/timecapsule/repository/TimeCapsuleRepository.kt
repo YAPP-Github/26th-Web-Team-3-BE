f7f413e849720e4d13d0a910e7a05155836b3747
Jhzlo <kjh0010703@naver.com>
2025-08-08 15:46:40 +0900

    chore: rollback

--- src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleRepository.kt
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleRepository.kt
@@ -1,6 +1,8 @@
 package com.yapp.lettie.domain.timecapsule.repository
 
 import com.yapp.lettie.domain.timecapsule.entity.TimeCapsule
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -9,6 +11,17 @@ import java.time.LocalDateTime
 interface TimeCapsuleRepository :
     JpaRepository<TimeCapsule, Long>,
     TimeCapsuleCustomerRepository {
+    @Query(
+        """
+    SELECT tc FROM TimeCapsule tc
+    LEFT JOIN Letter l ON l.timeCapsule.id = tc.id
+    WHERE  tc.accessType = 'PUBLIC'
+    GROUP BY tc.id
+    ORDER BY COUNT(l.id) DESC, tc.createdAt DESC
+    """,
+    )
+    fun findPopularTimeCapsules(pageable: Pageable): Page<TimeCapsule>
+
     @Query(
         """
         SELECT tc FROM TimeCapsule tc
