cb85e59b616ed4eb9d591695299590dd999b0215
Jhzlo <kjh0010703@naver.com>
2025-08-18 23:56:05 +0900

    refactor: status ACTIVE일 때만 메일 전송

--- src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleUserCustomRepositoryImpl.kt
+++ src/main/kotlin/com/yapp/lettie/domain/timecapsule/repository/TimeCapsuleUserCustomRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.yapp.lettie.domain.timecapsule.dto.RecipientRow
 import com.yapp.lettie.domain.timecapsule.entity.QTimeCapsuleUser
+import com.yapp.lettie.domain.timecapsule.entity.vo.TimeCapsuleUserStatus
 import com.yapp.lettie.domain.user.entity.QUser
 
 class TimeCapsuleUserCustomRepositoryImpl(
@@ -24,7 +25,10 @@ class TimeCapsuleUserCustomRepositoryImpl(
             )
             .from(timeCapsuleUser)
             .join(timeCapsuleUser.user, user)
-            .where(timeCapsuleUser.timeCapsule.id.`in`(capsuleIds))
+            .where(
+                timeCapsuleUser.timeCapsule.id.`in`(capsuleIds)
+                    .and(timeCapsuleUser.status.eq(TimeCapsuleUserStatus.ACTIVE)),
+            )
             .fetch()
     }
 }
