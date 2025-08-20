8d06e31a4670e789570c80cf7fe4331fd0424f3e
Jhzlo <kjh0010703@naver.com>
2025-08-20 23:26:11 +0900

    chore: test 컨트롤러 dev, prod에서 노출 제거

--- src/main/kotlin/com/yapp/lettie/api/email/controller/EmailController.kt
+++ src/main/kotlin/com/yapp/lettie/api/email/controller/EmailController.kt
@@ -2,6 +2,7 @@ package com.yapp.lettie.api.email.controller
 
 import com.yapp.lettie.api.email.service.EmailService
 import com.yapp.lettie.common.dto.ApiResponse
+import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/mail")
+@Profile("!(dev | prod)")
 class EmailController(
     private val emailService: EmailService,
 ) {
--- src/main/kotlin/com/yapp/lettie/common/controller/ExceptionTestController.kt
+++ src/main/kotlin/com/yapp/lettie/common/controller/ExceptionTestController.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Profile("!prod")
+@Profile("!(dev | prod)")
 @RestController
 class ExceptionTestController {
     private val log = KotlinLogging.logger {}
